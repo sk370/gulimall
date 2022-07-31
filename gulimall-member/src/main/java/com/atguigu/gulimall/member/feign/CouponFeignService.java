5aa667de27d4abb045181ff6eba7f300dcc5059c
朱宇琦（Zhu Yuqi） <icerivericeriver@hotmail.com>
2022-07-31 11:45:37 +0800

    1. 引入springboot alibaba 2. 使用nacos作为注册中心、配置中心 3. 使用spring cloud feign做请求处理 4. 修改spring boot、spring cloud版本适配本项目

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.member.feign;
+
+import com.atguigu.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author: INFINITY https://developer.aliyun.com/profile/sagwrxp2ua66w
+ * @date: 2022/7/30 17:39
+ */
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R membercoupons();
+}
