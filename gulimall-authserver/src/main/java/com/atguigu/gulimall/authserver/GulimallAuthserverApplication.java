c46d2ae8002113bf8edfe62029e107b867e78425
朱宇琦（Zhu Yuqi） <icerivericeriver@hotmail.com>
2022-11-02 10:50:51 +0800

    使用线程池异步编排详情页信息获取

---
+++ gulimall-authserver/src/main/java/com/atguigu/gulimall/authserver/GulimallAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallAuthserverApplication.class, args);
+	}
+
+}
